02aa86f5f1c6edfacd78b43acafb35e20177e53f
Tony Bendelow <tony.bendelow@trimano.com>
2016-10-30 14:35:35 +0000

    Simple project with failing Add Item test

---
+++ src/com/example/billingservice/TestBillingService.java
@@ -0,0 +1,14 @@
+package com.example.billingservice;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestBillingService {
+
+	@Test
+	public void testAdditem() {
+		fail("Not yet implemented");
+	}
+
+}
